492dc0bd5aac99d6d1383d28bffd93ea013b2bfb
Raheim <raheimbbailey@gmail.com>
2020-05-06 15:26:23 -0400

    reformat get request into function, fix comment posting, add comment clearing

--- sprint-3/javascript/comments.js
+++ sprint-3/javascript/comments.js
@@ -21,11 +21,9 @@ let submitBtn  = document.querySelector(".comments__btn");
 
 // console.log(images);
 
-
-
-    let p = 
-axios.get('https://project-1-api.herokuapp.com/comments?api_key=7424395f-e2a4-4e35-b456-c800d11c2d63')
-p.then(response => {
+let getPromise = () => {
+ return  axios.get('https://project-1-api.herokuapp.com/comments?api_key=7424395f-e2a4-4e35-b456-c800d11c2d63')
+.then(response => {
     console.log("you got it:", response.data);
 
     let usersData = JSON.stringify(response.data);
@@ -34,113 +32,22 @@ p.then(response => {
     let newUsersData = JSON.parse(usersData);
     console.log(newUsersData);
 
-    
-
     newUsersData.forEach((item) => {
         let commentSpace = displayComment(item);
-        userCommentCtn.appendChild(commentSpace);
-    });
-
-    
+        userCommentCtn.prepend(commentSpace);
+    }) 
 })
-p.catch(error => {
+// .then(() => {
+//     let userSpace = document.querySelector(".user__comments-ctn");
+//     userSpace.innerHTML = "";
+// })
+.catch(error => {
     console.log("you didnt get the data", error)
 })
-
-const images = [
-    {
-    //  name:"Micheal Lyons", 
-    //  timestamp:"12/18/2018",
-    //  comment: "They BLEW the ROOF off their last show, once everyone started figuring out they were going. This is still simply the greates opening of a concert I have EVER wistnessed",
-     pic: '../../assets/images/spirit-animal-1.jpg'
-    },
-    {
-    //  name:"Gary Wong",
-    //  timestamp: "12/12/2018",
-    //  comment: "Everytime I see him shred I feel so motivated to get off my couch and hop on my board. Hw's so talented! I wish I can ride like him one day so I can really enjoy myself!",
-     pic: '../../assets/images/puppy-2.jpg'   
-    },
-    {
-    //  name: "Theodore Duncan ",
-    //  timestamp: "11/15/2018",
-    //  comment: "How can someone be so good!!! Yo can tell he lives for this and loves to do it everyday. Everytime I see him I feel instantly happy! He's definitely my favorite ever",   
-     pic: '../../assets/images/puppy-1.jpg'
-    }
-];
-// images.forEach((pic) => {
-//     let commentSpace = displayComment(pic);
-//     userCommentCtn.appendChild(commentSpace);
-// });
-
-
-
-// Generate Comments HTML
-let displayComment = (newUsersData) => {
-
-
-
-    // Add Div for Comments to page
-    let commentSpace = document.createElement('div');
-    commentSpace.classList.add("gen-comment");
-
-    let userName = document.createElement("h4");
-    let userTime = document.createElement("p");
-    let userComment = document.createElement("p");
-    let imageCtn = document.createElement("div");
-    let userImage = document.createElement("img");
-    let commentInfoCtn =  document.createElement("div");
-    commentInfoCtn.classList.add("gen-comment__ctn");
-
-    userName.innerText = newUsersData.name;
-    userName.classList.add("users__name");
-    commentInfoCtn.appendChild(userName);
-    commentSpace.appendChild(commentInfoCtn);
-    console.log(userName);
-
-    let timestamp = newUsersData.timestamp;
-    let dateObj = new Date (timestamp);
-    let month = dateObj.getMonth() + 1;
-    let year = dateObj.getFullYear();
-    let date = dateObj.getDate();
-
-    userTime.innerText = `${month}/${date}/${year}`;
-    userTime.classList.add("users__time");
-    commentInfoCtn.appendChild(userTime);
-    commentSpace.appendChild(commentInfoCtn);
-    console.log(userTime);
-    
-    userComment.innerText = newUsersData.comment;
-    userComment.classList.add("users__comment");
-    commentInfoCtn.appendChild(userComment);
-    commentSpace.appendChild(commentInfoCtn);
-    console.log(userComment);
-
-    
-    userImage.src = newUsersData.pic;
-    userImage.classList.add("user-image");
-    imageCtn.classList.add("img-ctn");
-    imageCtn.appendChild(userImage);
-    commentSpace.appendChild(imageCtn);
-
-    return commentSpace;
 }
 
+getPromise();
 
-
-// let data = 
-// axios.get('https://project-1-api.herokuapp.com/comments?api_key=7424395f-e2a4-4e35-b456-c800d11c2d63')
-// p.then(response => {
-//     console.log("you got it:", response.data);
-
-//     let usersData = JSON.stringify(response.data);
-//     // console.log(usersData);
-
-//     let newUsersData = JSON.parse(usersData);
-//     console.log(newUsersData);
-// })
-// p.catch(error => {
-//     console.log("you didnt get the data", error)
-// })
 
 
 
@@ -175,38 +82,107 @@ formInfoCtn.appendChild(commentBtn);
 commentForm.appendChild(formInfoCtn);
 
 
-// Add event listener to comment form which created new Comment object and prepends to page with random image
+
 commentForm.addEventListener('submit', e => {
     e.preventDefault();
+    let userSpace = document.querySelector(".user__comments-ctn");
+    userSpace.innerHTML = "";
     // Define an array with image fil paths
-    let ranUserImg = ['../../assets/images/puppy-3.jpg', '../../assets/images/puppy-4.jpg',
-    '../../assets/images/puppy-5.jpg', '../../assets/images/cat-1.jpg', '../../assets/images/cat-2.jpg',
-    '../../assets/images/cat-3.jpg'    
-    ];
+    // let ranUserImg = ['../../assets/images/puppy-3.jpg', '../../assets/images/puppy-4.jpg',
+    // '../../assets/images/puppy-5.jpg', '../../assets/images/cat-1.jpg', '../../assets/images/cat-2.jpg',
+    // '../../assets/images/cat-3.jpg'    
+    // ];
     // write a function that takes the image array and runs Math.'' to select a random image
     // Function will return the image
     ranImg = (imgArr) => {
         return imgArr[Math.floor(Math.random() * imgArr.length)];
     }
+    
     let newUser = e.target.userName.value;
     let newComment = e.target.userVoice.value;
-    let commentDate = new Date();
-    // variable to hold the function invoked on my random user image array
-    let newUserImg = ranImg(ranUserImg);
 
-    let newCommentData = {
-        name: newUser,
-        timestamp: (commentDate.getMonth()+1)+'/'+commentDate.getDate()+'/'+commentDate.getFullYear(),
-        comment: newComment,
-        // new comment Data Key value pair to hold function "is this a method?"
-        image: newUserImg
-    };
+    axios
+    .post('https://project-1-api.herokuapp.com/comments?api_key=7424395f-e2a4-4e35-b456-c800d11c2d63',{
+            "name": newUser,
+            "comment": newComment,
+            })
+            
+            .then(result => {
+                
+                let newCommentData = result.data;
+                
+                let commentSpace = displayComment(newCommentData);
+                userCommentCtn.prepend(commentSpace);
+            })
+            event.target.reset();
+            getPromise();    
+        });
 
-    comments.push(newCommentData);
-    sessionStorage.clear();
-    let commentSpace = displayComment(newCommentData);
-    userCommentCtn.prepend(commentSpace);
 
-    event.target.reset();
-});
 
+// const images = [
+//     {
+    
+//      pic: '../../assets/images/spirit-animal-1.jpg'
+//     },
+//     {
+   
+//      pic: '../../assets/images/puppy-2.jpg'   
+//     },
+//     {
+   
+//      pic: '../../assets/images/puppy-1.jpg'
+//     }
+// ];
+
+
+
+
+
+
+
+// Generate Comments HTML
+let displayComment = (newUsersData) => {
+
+    // Add Div for Comments to page
+    let commentSpace = document.createElement('div');
+    commentSpace.classList.add("gen-comment");
+
+    let userName = document.createElement("h4");
+    let userTime = document.createElement("p");
+    let userComment = document.createElement("p");
+    let imageCtn = document.createElement("div");
+    let userImage = document.createElement("img");
+    let commentInfoCtn =  document.createElement("div");
+    commentInfoCtn.classList.add("gen-comment__ctn");
+
+    userName.innerText = newUsersData.name;
+    userName.classList.add("users__name");
+    commentInfoCtn.appendChild(userName);
+    commentSpace.appendChild(commentInfoCtn);
+
+    let timestamp = newUsersData.timestamp;
+    let dateObj = new Date (timestamp);
+    let month = dateObj.getMonth() + 1;
+    let year = dateObj.getFullYear();
+    let date = dateObj.getDate();
+
+    userTime.innerText = `${month}/${date}/${year}`;
+    userTime.classList.add("users__time");
+    commentInfoCtn.appendChild(userTime);
+    commentSpace.appendChild(commentInfoCtn);
+    
+    userComment.innerText = newUsersData.comment;
+    userComment.classList.add("users__comment");
+    commentInfoCtn.appendChild(userComment);
+    commentSpace.appendChild(commentInfoCtn);
+
+    
+    userImage.src = newUsersData.pic;
+    userImage.classList.add("user-image");
+    imageCtn.classList.add("img-ctn");
+    imageCtn.appendChild(userImage);
+    commentSpace.appendChild(imageCtn);
+
+    return commentSpace;
+}
